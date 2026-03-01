2172ea5325824507ccc41145fa45453ed3cf8850
Luiz Felipe <luizfelipepintorr@gmail.com>
2026-03-01 18:33:36 -0300

    configurando os endpoints

--- src/resources/image/imageService.ts
+++ src/resources/image/imageService.ts
@@ -1,7 +1,7 @@
 import {Image} from '@/resources/image/imageResource'
 
 class ImageService{
-    baseURL: string = 'http://localhost:8080/v1/images';
+    baseURL: string = `${process.env.NEXT_PUBLIC_API_URL}/v1/images`;
     
     async buscar(query: string, extension: string) : Promise<Image[]>{
         const url = `${this.baseURL}?query=${query}&extension=${extension}`
